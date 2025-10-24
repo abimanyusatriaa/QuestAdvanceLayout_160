be24eaa96b6a0b6e6e6afca02e3d3096f80270e9
abimanyusatriaa <adji3890@gmail.com>
2025-10-24 10:14:12 +0700

    membuat spacer

--- app/src/main/java/com/example/prak3/UIpertama.kt
+++ app/src/main/java/com/example/prak3/UIpertama.kt
@@ -61,4 +61,5 @@ fun ActivitasPertama(modifier: Modifier){
                         .size(size = 100.dp)
                         .padding(all = 5.dp)
                 )
+                Spacer(modifier = Modifier.width( width = 30.dp))
 
