b10518d527033125d715a85b1dd3a34fd4b95b66
abimanyusatriaa <adji3890@gmail.com>
2025-10-25 17:58:43 +0700

    membuat icon kiri

--- app/src/main/java/com/example/prak3/TugasPraktikum.kt
+++ app/src/main/java/com/example/prak3/TugasPraktikum.kt
@@ -117,5 +117,20 @@ fun TugasPraktikum(modifier: Modifier) {
                     .padding(horizontal = 16.dp, vertical = 12.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween // jarak kiri-kanan
-            )
+            ) {
+                // Bagian kiri (ikon + teks)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    val iconSize = 50.dp
+
+                    Image(
+                        painter = painterResource(id = R.drawable.privasi),
+                        contentDescription = "Privasi",
+                        modifier = Modifier
+                            .size(iconSize)
+                            .padding(4.dp)
+                    )
+
+
 }
